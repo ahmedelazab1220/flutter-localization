0a94d595954c0cfe0a96da32ba3f947ddb723dff
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-10-13 17:25:08 +0300

    Add Toggle Button To Switch Language.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:localization/generated/l10n.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +13,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      locale: const Locale('ar'),
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => Localization(),
+      child: Consumer<Localization>(
+        builder: (context, Localization localization, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            locale: Locale(localization.locale),
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -48,16 +56,29 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(S.of(context).title),
       ),
-      body: Row(
+      body: Column(
         children: [
-          Text(S.of(context).hello),
-          Padding(
-            padding: EdgeInsets.only(
-              left: isArabic() ? 0 : 8,
-              right: isArabic() ? 8 : 0,
-            ),
-            child: Text(S.of(context).name),
+          Row(
+            children: [
+              Text(S.of(context).hello),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: isArabic() ? 0 : 8,
+                  right: isArabic() ? 8 : 0,
+                ),
+                child: Text(S.of(context).name),
+              ),
+            ],
           ),
+          Center(
+            child: Switch(
+              value: isArabic(),
+              onChanged: (value) {
+                Provider.of<Localization>(context, listen: false)
+                    .setLocale(isArabic() ? 'en' : 'ar');
+              },
+            ),
+          )
         ],
       ),
     );
@@ -66,4 +87,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
 bool isArabic() {
   return Intl.getCurrentLocale() == 'ar';
+}
+
+class Localization extends ChangeNotifier {
+  String locale = 'en';
+
+  void setLocale(String newLocale) {
+    locale = newLocale;
+    notifyListeners();
+  }
 }
